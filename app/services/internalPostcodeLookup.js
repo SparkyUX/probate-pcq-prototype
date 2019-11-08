ccf4127c212f8a244ac6aed67848d559ca369cc5
Jen Sparks <jen.sparks@hmcts.net>
2019-11-08 12:30:20 +0000

    postcode finder fixed

--- app/services/internalPostcodeLookup.js
+++ app/services/internalPostcodeLookup.js
@@ -3,9 +3,9 @@ const {map, chain, isNull, toLower, upperFirst} = require('lodash')
 const mockData = require('./postcodeLookupMockData.json')
 
 const addressFields = [
-//  'organisation_name',
-//  'department_name',
-//  'po_box_number',
+  'organisation_name',
+  'department_name',
+  'po_box_number',
   'building_name',
   'sub_building_name',
   'building_number',
@@ -16,7 +16,7 @@ const addressFields = [
   'post_town'
 ]
 
-const token = () => {
+const token = function() {
   return process.env.POST_CODE_ACCESS_TOKEN
 }
 
@@ -24,6 +24,8 @@ module.exports = {
   token: token,
   lookup: (postcode) => {
     if (token()) {
+      console.log('token ' + token)
+
       return request({
         url: `https://postcodeinfo.service.justice.gov.uk/addresses/?postcode=${postcode}`,
         headers: {'Authorization': process.env.POST_CODE_ACCESS_TOKEN}
@@ -39,6 +41,7 @@ module.exports = {
           .value())
         )
     }
+    console.log('no token ' + token)
     return (new Promise((resolve, reject) => resolve(mockData)))
   }
 }
